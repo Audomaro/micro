5b603df68ad5bb57904c0c71ca5317805e413b93
Audomaro Glez <jose_audomaro@hotmail.com>
2022-05-02 09:37:01 -0500

    Styles update

--- apps/host/src/app/app.module.ts
+++ apps/host/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { UiHeaderModule } from '@micro/ui/header';
+import { UiSideMenuModule } from '@micro/ui/side-menu';
 
 import { AppComponent } from './app.component';
-import { UiHeaderModule } from '@micro/ui/header';
-import { RouterModule } from '@angular/router';
+
 // import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -11,8 +13,9 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     // AppRoutingModule,
-    UiHeaderModule,
     RouterModule,
+    UiHeaderModule,
+    UiSideMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
